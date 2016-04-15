ca0b03381bd7baa94f0872fa052562b70dabda3c
hammi <hammi@10.0.0.17>
2016-04-15 19:34:40 -0400

    Beginning

---
+++ semesterproject/src/main/java/edu/cis232/semesterproject/App.java
@@ -0,0 +1,13 @@
+package edu.cis232.semesterproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Cash Register!" );
+    }
+}
