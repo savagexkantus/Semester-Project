7965faa88a419584e06ad0f22178307170508b4c
SK207_34 <SK207_34@K207_W834.carrollcc.edu>
2016-04-27 18:45:58 -0400

    Requirement 1 done

--- semesterproject/src/main/java/edu/cis232/semesterproject/App.java
+++ semesterproject/src/main/java/edu/cis232/semesterproject/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Cash Register!" );
+    	System.out.println("Semester Project");
+    	System.out.println("Alex Carroll");
+    	System.out.println("Brad Wilfong");
+    	
+        System.out.println( "Something" );
+        
     }
 }
