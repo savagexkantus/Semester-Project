87be3b9bd415b444f4427151fe6015a44f7a8308
SK207_13 <SK207_13@K207_W813.carrollcc.edu>
2016-05-09 20:11:26 -0400

    made the genre things

--- semesterproject/src/main/java/edu/cis232/semesterproject/CustomerMenu.java
+++ semesterproject/src/main/java/edu/cis232/semesterproject/CustomerMenu.java
@@ -6,18 +6,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class CustomerMenu extends Application{
-
-    @FXML
+	
+	ChildrensBook gatordad = new ChildrensBook("Gator Dad", "5", "200", "Brian Lies", "5.47", "5.99", "Children", 3, 6);
+    ChildrensBook redfoxes = new ChildrensBook("Red Foxes", "7", "270", "Megan Borgert", ".75", "1.99", "Children", 2, 8);
+    
+    AdultBook thelastmile = new AdultBook("The Last Mile", "4", "220", "David Baldacci", "6.70", "17.50", "Fiction", 1, false);
+    AdultBook falloflight = new AdultBook("Fall of Light", "3", "221", "Steven Erikson", "14.00", "19.99", "Fiction", 5, true);
+    AdultBook wildelake = new AdultBook("Wilde Lake", "2", "241", "Laura Lippman", "15.60", "22.99", "Non Fiction", 2, false);
+    AdultBook cityoflostsouls = new AdultBook("City of Lost Souls", "1", "111", "Cassandra Claire", "15.20", "20.99", "Fiction", 3, true);
+   
+	
+	@FXML
     private Button SearchButton;
-
-    @FXML
-    private Button AdultButton;
 
     @FXML
     private RadioButton radio1;
@@ -38,11 +45,44 @@ public class CustomerMenu extends Application{
     private ImageView MonthImage;
 
     @FXML
-    private Button YouthButton;
-
-    @FXML
     private ImageView backButton;
 
+    @FXML
+    private Label gatorDad;
+
+    @FXML
+    private Label lastMile;
+
+    @FXML
+    private Label citySouls;
+
+    @FXML
+    private Label redFoxes;
+
+    @FXML
+    private Label wildLake;
+
+    @FXML
+    private Label fallofLight;
+
+    @FXML
+    private Label gatorLabel;
+
+    @FXML
+    private Label lastLabel;
+
+    @FXML
+    private Label cityLabel;
+
+    @FXML
+    private Label foxLabel;
+
+    @FXML
+    private Label wildLabel;
+
+    @FXML
+    private Label fallofLabel;
+    
     @FXML
     void ClickAdult() {
 
@@ -123,6 +163,35 @@ public class CustomerMenu extends Application{
       			
       		}
 
+    }
+    @FXML
+    void fallclick() {
+    	fallofLabel.setText(falloflight.getGenre());
+    }
+
+    @FXML
+    void foxclick() {
+    	foxLabel.setText(redfoxes.getGenre());
+    }
+
+    @FXML
+    void lastclick() {
+    	lastLabel.setText(thelastmile.getGenre());
+    }
+    
+    @FXML
+    void cityclick() {
+    	cityLabel.setText(cityoflostsouls.getGenre());
+    }
+    
+    @FXML
+    void Gatorclick() {
+    	gatorLabel.setText(gatordad.getGenre());
+    }
+    
+    @FXML
+    void wildclick() {
+    	wildLabel.setText(wildelake.getGenre());
     }
 
 	@Override
